5f222a38afea113e7686101ef294c192b5328675
himasahamed <himasahamet@gmail.com>
2026-05-19 12:04:17 +0530

    Added direct Supabase URL and Anon Key to supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://taworiusfdipiegepbcw.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhd29yaXVzZmRpcGllZ2VwYmN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxNTIzNDgsImV4cCI6MjA5NDcyODM0OH0.8mUci2WInVmBEcyA7Y-_4y5MpF-PZGFBe5ZAd14dPB8'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
